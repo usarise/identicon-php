5aef8817cc82bb201a164bed48e538ce57cb767e
usarise <7043681+usarise@users.noreply.github.com>
2023-07-24 14:48:17 +0700

    add tests binary resolution

--- tests/BinaryTest.php
+++ tests/BinaryTest.php
@@ -34,7 +34,7 @@ final class BinaryTest extends TestCase {
         );
     }
 
-    public function testBinStr(): void {
+    public function testBinStrDefault(): void {
         $binary = new Binary(Resolution::Medium);
 
         $this->assertEquals(
@@ -45,7 +45,62 @@ final class BinaryTest extends TestCase {
         );
     }
 
-    public function testPixels(): void {
+    public function testBinStrResolutionTiny(): void {
+        $binary = new Binary(Resolution::Tiny);
+
+        $this->assertEquals(
+            '01001011010000111011000010101110',
+            $binary->getBinStr(
+                $binary->getBytes('r'),
+            ),
+        );
+    }
+
+    public function testBinStrResolutionSmall(): void {
+        $binary = new Binary(Resolution::Small);
+
+        $this->assertEquals(
+            '01001011010000111011000010101110111000110101011000',
+            $binary->getBinStr(
+                $binary->getBytes('r'),
+            ),
+        );
+    }
+
+    public function testBinStrResolutionMedium(): void {
+        $binary = new Binary(Resolution::Medium);
+
+        $this->assertEquals(
+            '010010110100001110110000101011101110001101010110001001001100110110010101',
+            $binary->getBinStr(
+                $binary->getBytes('r'),
+            ),
+        );
+    }
+
+    public function testBinStrResolutionLarge(): void {
+        $binary = new Binary(Resolution::Large);
+
+        $this->assertEquals(
+            '01001011010000111011000010101110111000110101011000100100110011011001010110111001000100000001100010',
+            $binary->getBinStr(
+                $binary->getBytes('r'),
+            ),
+        );
+    }
+
+    public function testBinStrResolutionHuge(): void {
+        $binary = new Binary(Resolution::Huge);
+
+        $this->assertEquals(
+            '01001011010000111011000010101110111000110101011000100100110011011001010110111001000100000001100010011011001111011100001000110001',
+            $binary->getBinStr(
+                $binary->getBytes('r'),
+            ),
+        );
+    }
+
+    public function testPixelsDefault(): void {
         $binary = new Binary(Resolution::Medium);
 
         $this->assertEquals(
@@ -66,6 +121,136 @@ final class BinaryTest extends TestCase {
             $binary->getPixels(
                 $binary->getBinStr(
                     $binary->getBytes('test'),
+                ),
+            ),
+        );
+    }
+
+    public function testPixelsResolutionTiny(): void {
+        $binary = new Binary(Resolution::Tiny);
+
+        $this->assertEquals(
+            [
+                [0, 0, 0, 0, 0, 0, 0, 0],
+                [0, 1, 0, 0, 0, 0, 1, 0],
+                [0, 0, 1, 1, 0, 0, 0, 0],
+                [0, 0, 1, 0, 1, 1, 1, 0],
+                [0, 0, 1, 0, 1, 1, 1, 0],
+                [0, 0, 1, 1, 0, 0, 0, 0],
+                [0, 1, 0, 0, 0, 0, 1, 0],
+                [0, 0, 0, 0, 0, 0, 0, 0],
+            ],
+            $binary->getPixels(
+                $binary->getBinStr(
+                    $binary->getBytes('r'),
+                ),
+            ),
+        );
+    }
+
+    public function testPixelsResolutionSmall(): void {
+        $binary = new Binary(Resolution::Small);
+
+        $this->assertEquals(
+            [
+                [0, 0, 0, 0, 0, 0, 0, 0, 0, 0],
+                [0, 0, 0, 0, 1, 1, 1, 0, 1, 0],
+                [0, 0, 0, 0, 1, 0, 1, 0, 1, 0],
+                [0, 0, 1, 1, 1, 0, 0, 0, 1, 0],
+                [0, 1, 0, 1, 0, 1, 1, 0, 0, 0],
+                [0, 1, 0, 1, 0, 1, 1, 0, 0, 0],
+                [0, 0, 1, 1, 1, 0, 0, 0, 1, 0],
+                [0, 0, 0, 0, 1, 0, 1, 0, 1, 0],
+                [0, 0, 0, 0, 1, 1, 1, 0, 1, 0],
+                [0, 0, 0, 0, 0, 0, 0, 0, 0, 0],
+            ],
+            $binary->getPixels(
+                $binary->getBinStr(
+                    $binary->getBytes('r'),
+                ),
+            ),
+        );
+    }
+
+    public function testPixelsResolutionMedium(): void {
+        $binary = new Binary(Resolution::Medium);
+
+        $this->assertEquals(
+            [
+                [0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0],
+                [0, 0, 1, 1, 1, 0, 1, 1, 0, 0, 0, 0],
+                [0, 0, 1, 0, 1, 1, 1, 0, 1, 1, 1, 0],
+                [0, 0, 1, 1, 0, 1, 0, 1, 0, 1, 1, 0],
+                [0, 0, 1, 0, 0, 1, 0, 0, 1, 1, 0, 0],
+                [0, 1, 0, 1, 1, 0, 0, 1, 0, 1, 0, 0],
+                [0, 1, 0, 1, 1, 0, 0, 1, 0, 1, 0, 0],
+                [0, 0, 1, 0, 0, 1, 0, 0, 1, 1, 0, 0],
+                [0, 0, 1, 1, 0, 1, 0, 1, 0, 1, 1, 0],
+                [0, 0, 1, 0, 1, 1, 1, 0, 1, 1, 1, 0],
+                [0, 0, 1, 1, 1, 0, 1, 1, 0, 0, 0, 0],
+                [0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0],
+            ],
+            $binary->getPixels(
+                $binary->getBinStr(
+                    $binary->getBytes('r'),
+                ),
+            ),
+        );
+    }
+
+    public function testPixelsResolutionLarge(): void {
+        $binary = new Binary(Resolution::Large);
+
+        $this->assertEquals(
+            [
+                [0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0],
+                [0, 1, 1, 0, 1, 1, 0, 0, 0, 0, 1, 0, 1, 0],
+                [0, 1, 1, 0, 1, 1, 1, 0, 0, 0, 1, 1, 0, 0],
+                [0, 1, 0, 1, 1, 0, 0, 0, 1, 0, 0, 1, 0, 0],
+                [0, 1, 0, 0, 1, 1, 0, 1, 1, 0, 0, 1, 0, 0],
+                [0, 1, 1, 0, 1, 1, 1, 0, 0, 1, 0, 0, 0, 0],
+                [0, 0, 0, 0, 0, 0, 0, 1, 1, 0, 0, 0, 1, 0],
+                [0, 0, 0, 0, 0, 0, 0, 1, 1, 0, 0, 0, 1, 0],
+                [0, 1, 1, 0, 1, 1, 1, 0, 0, 1, 0, 0, 0, 0],
+                [0, 1, 0, 0, 1, 1, 0, 1, 1, 0, 0, 1, 0, 0],
+                [0, 1, 0, 1, 1, 0, 0, 0, 1, 0, 0, 1, 0, 0],
+                [0, 1, 1, 0, 1, 1, 1, 0, 0, 0, 1, 1, 0, 0],
+                [0, 1, 1, 0, 1, 1, 0, 0, 0, 0, 1, 0, 1, 0],
+                [0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0],
+            ],
+            $binary->getPixels(
+                $binary->getBinStr(
+                    $binary->getBytes('r'),
+                ),
+            ),
+        );
+    }
+
+    public function testPixelsResolutionHuge(): void {
+        $binary = new Binary(Resolution::Huge);
+
+        $this->assertEquals(
+            [
+                [0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0],
+                [0, 0, 1, 1, 0, 0, 0, 0, 1, 0, 1, 0, 1, 1, 1, 0],
+                [0, 1, 1, 0, 0, 0, 1, 1, 0, 1, 0, 1, 0, 1, 1, 0],
+                [0, 0, 1, 0, 0, 1, 0, 0, 1, 1, 0, 0, 1, 1, 0, 0],
+                [0, 0, 0, 1, 0, 1, 0, 1, 1, 0, 1, 1, 1, 0, 0, 0],
+                [0, 0, 0, 1, 0, 0, 0, 0, 0, 0, 0, 1, 1, 0, 0, 0],
+                [0, 0, 0, 1, 1, 0, 1, 1, 0, 0, 1, 1, 1, 1, 0, 0],
+                [0, 1, 0, 0, 0, 0, 1, 0, 0, 0, 1, 1, 0, 0, 0, 0],
+                [0, 1, 0, 0, 0, 0, 1, 0, 0, 0, 1, 1, 0, 0, 0, 0],
+                [0, 0, 0, 1, 1, 0, 1, 1, 0, 0, 1, 1, 1, 1, 0, 0],
+                [0, 0, 0, 1, 0, 0, 0, 0, 0, 0, 0, 1, 1, 0, 0, 0],
+                [0, 0, 0, 1, 0, 1, 0, 1, 1, 0, 1, 1, 1, 0, 0, 0],
+                [0, 0, 1, 0, 0, 1, 0, 0, 1, 1, 0, 0, 1, 1, 0, 0],
+                [0, 1, 1, 0, 0, 0, 1, 1, 0, 1, 0, 1, 0, 1, 1, 0],
+                [0, 0, 1, 1, 0, 0, 0, 0, 1, 0, 1, 0, 1, 1, 1, 0],
+                [0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0],
+            ],
+            $binary->getPixels(
+                $binary->getBinStr(
+                    $binary->getBytes('r'),
                 ),
             ),
         );
