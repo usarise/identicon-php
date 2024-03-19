676ca692997b8b2295f5180ad856b0c89132c8f5
usarise <7043681+usarise@users.noreply.github.com>
2024-03-19 19:37:40 +0700

    cleanup

--- tests/Color/ColorTest.php
+++ tests/Color/ColorTest.php
@@ -34,15 +34,6 @@ final class ColorTest extends TestCase {
             $color->foreground,
         );
 
-        $color = new Color();
-
-        $this->assertNull(
-            $color->background,
-        );
-        $this->assertNull(
-            $color->foreground,
-        );
-
         $color = new Color(
             '#f2f1f2',
             '#55c878',
